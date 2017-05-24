6fd5873c8f8bd969e17bcfccd630733ff0b644e0
Andela-eugene <eugene.liyai@andela.com>
2017-05-24 16:42:08 +0300

    [Feature] add side bar dynamic image

---
+++ the_hive/static/js/jquery-sidebar-animation.js
@@ -0,0 +1,49 @@
+/**
+ * Created by eugeneliyai on 23/05/2017.
+ */
+/**
+ * Created by Kupletsky Sergey on 08.09.14.
+ *
+ * Add JQuery animation to bootstrap dropdown elements.
+ */
+
+(function($) {
+    var dropdown = $('.dropdown');
+
+    // Add slidedown animation to dropdown
+    dropdown.on('show.bs.dropdown', function(e){
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    });
+
+    // Add slideup animation to dropdown
+    dropdown.on('hide.bs.dropdown', function(e){
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    });
+})(jQuery);
+
+
+
+(function(removeClass) {
+
+    jQuery.fn.removeClass = function( value ) {
+		if ( value && typeof value.test === "function" ) {
+			for ( var i = 0, l = this.length; i < l; i++ ) {
+				var elem = this[i];
+				if ( elem.nodeType === 1 && elem.className ) {
+					var classNames = elem.className.split( /\s+/ );
+
+					for ( var n = classNames.length; n--; ) {
+						if ( value.test(classNames[n]) ) {
+							classNames.splice(n, 1);
+						}
+					}
+					elem.className = jQuery.trim( classNames.join(" ") );
+				}
+			}
+		} else {
+			removeClass.call(this, value);
+		}
+		return this;
+	}
+
+})(jQuery.fn.removeClass);
--- the_hive/static/js/settings-toggle.js
+++ the_hive/static/js/settings-toggle.js
@@ -1,14 +1,99 @@
-$(function(){
+// Sidebar constructor
+//
+// -------------------
+$(document).ready(function() {
+    var host = document.location.origin;
+    var overlay = $('.sidebar-overlay');
 
-	$('#slide-submenu').on('click',function() {			        
-        $(this).closest('.list-group').fadeOut('slide',function(){
-        	$('.mini-submenu').fadeIn();	
-        });
-        
-      });
+    var sidebar = $('#sidebar');
+    var sidebarHeader = $('#sidebar .sidebar-header');
+    var sidebarImg = sidebarHeader.css('background-image');
+    var toggleButtons = $('.sidebar-toggle');
 
-	$('.mini-submenu').on('click',function(){		
-        $(this).next('.list-group').toggle('slide');
-        $('.mini-submenu').hide();
-	})
-})
+    $sidebar = $('.sidebar-nav');
+    $sidebar_img_container = $sidebar.find('.sidebar-background');
+
+
+    // Sidebar position
+    $('#sidebar-position').change(function() {
+        var value = $( this ).val();
+        sidebar.removeClass('sidebar-fixed-left sidebar-fixed-right sidebar-stacked').addClass(value).addClass('open');
+        if (value == 'sidebar-fixed-left' || value == 'sidebar-fixed-right') {
+            $('.sidebar-overlay').addClass('active');
+        }
+        // Show toggle buttons
+        if (value != '') {
+            toggleButtons.css('display', 'initial');
+            $('body').css('display', 'initial');
+        } else {
+            // Hide toggle buttons
+            toggleButtons.css('display', 'none');
+            $('body').css('display', 'table');
+        }
+    });
+
+    // Sidebar theme
+    $('#sidebar-theme').change(function() {
+        var value = $( this ).val();
+        console.log('Execute ...... '+value);
+        sidebar.removeClass('sidebar-default sidebar-inverse sidebar-colored sidebar-colored-inverse').addClass(value)
+    });
+
+    // Header cover
+    $('#sidebar-header').change(function() {
+        var value = $(this).val();
+
+        $('.sidebar-header').removeClass('header-cover').addClass(value);
+
+        if (value == 'header-cover') {
+            sidebarHeader.css('background-image', sidebarImg)
+        } else {
+            sidebarHeader.css('background-image', '')
+        }
+    });
+
+    // sidebar overlay function
+
+    $('.sidebar-toggle').on('click', function() {
+        var sidebar = $('#sidebar');
+        sidebar.toggleClass('open');
+        if ((sidebar.hasClass('sidebar-fixed-left') || sidebar.hasClass('sidebar-fixed-right')) && sidebar.hasClass('open')) {
+            overlay.addClass('active');
+        } else {
+            overlay.removeClass('active');
+        }
+    });
+
+    overlay.on('click', function() {
+        $(this).removeClass('active');
+        $('#sidebar').removeClass('open');
+    });
+
+    $('.fixed-plugin a').click(function(event){
+        if($(this).hasClass('switch-trigger')){
+            if(event.stopPropagation){
+                event.stopPropagation();
+            }
+            else if(window.event){
+               window.event.cancelBubble = true;
+            }
+        }
+    });
+
+    $('.fixed-plugin .img-holder').click(function(){
+        $full_page_background = $('.full-page-background');
+
+        $(this).parent('li').siblings().removeClass('active');
+        $(this).parent('li').addClass('active');
+
+
+        var new_image = host+$(this).find("img").attr('src');
+
+        if($('.switch-sidebar-image input:checked').length != 0){
+            $sidebar.fadeOut('fast', function () {
+                $sidebar.css('background-image',' linear-gradient( rgb(44, 62, 80), rgba(44, 62, 80, 0.4) ), url("' + new_image + '")');
+                $sidebar.fadeIn('fast');
+            })
+        }
+    });
+});
