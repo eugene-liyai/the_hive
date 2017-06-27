e7f03640409af74ad52e46c08bbe9dfba7dd37b3
Andela-eugene <eugene.liyai@andela.com>
2017-06-27 12:12:01 +0300

    [Feature] implement sidebar responsive to record query

--- the_hive/static/js/tables.js
+++ the_hive/static/js/tables.js
@@ -4,4 +4,17 @@ $(document).ready(function() {
     $('.hidden-row-style').css('padding', '0');
 
     $('#stats_table').DataTable();
+
+    $('select.form-control.input-sm').change(function(){
+         var x = $(this).val();
+         if (x === '10') {
+             $('ul.nav.sidebar-nav').css('height', '750px');
+         }else if (x === '25') {
+             $('ul.nav.sidebar-nav').css('height', '950px');
+         }else if (x === '50') {
+             $('ul.nav.sidebar-nav').css('height', '1850px');
+         }else if (x === '100'){
+             $('ul.nav.sidebar-nav').css('height', '3700px');
+         }
+    });
 });
