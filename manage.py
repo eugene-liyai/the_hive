c1aa83e3578f05d95d4066c8ae346dace5d0d073
Andela-eugene <eugene.liyai@andela.com>
2018-05-12 14:56:44 +0300

    [Chore: ] turn on debug

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@
 # ============================================================================
 # necessary imports
 # ============================================================================
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
 
@@ -20,6 +20,7 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server(debug=True))
 
 
 @manager.command
